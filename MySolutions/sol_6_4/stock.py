799889cb9c8c79ed3f967bb9f1dd7130e19e7572
SumanthRH <sumanthrh99@gmail.com>
2024-01-17 15:53:56 -0800

    7.1 update

--- MySolutions/sol_6_4/stock.py
+++ MySolutions/sol_6_4/stock.py
@@ -1,5 +1,6 @@
 from sol_6_4.structure import Structure
 import sys
+from sol_7_1.validate import validated
 
 class Stock(Structure):
     _fields = ("name", "shares", "price")
@@ -9,8 +10,10 @@ class Stock(Structure):
     @property
     def cost(self):
         return self.shares * self.price
-
+    
+    @validated
     def sell(self, nshares):
         self.shares -= nshares
+    # sell = ValidatedFunction(sell)
 
 Stock.create_init()
